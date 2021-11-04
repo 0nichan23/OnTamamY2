d711354ca425b562441d177fd86aaf0953b8b008
RonTheDragon <60644954+RonTheDragon@users.noreply.github.com>
2021-11-04 20:23:16 +0200

    Im very comfused

---
+++ example-stam/Assets/Scripts/Save.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Save : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = "Assets/Resources/test.txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Test");
+        //writer.Flush();
+        writer.Close();
+
+        string path2 = "Assets/Resources/test.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
