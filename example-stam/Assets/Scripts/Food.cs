1da8c5eca2544895c1021b794b99f5b801daa697
RonTheDragon <60644954+RonTheDragon@users.noreply.github.com>
2021-11-19 12:31:47 +0200

    oof

--- example-stam/Assets/Scripts/Food.cs
+++ example-stam/Assets/Scripts/Food.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour
-{ 
+{
+    public GameObject Particle;
+    MeshRenderer MR;
+    Color c;
+    bool Eaten;
+
+    private void Start()
+    {
+        MR = GetComponent<MeshRenderer>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+
+
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E)&&Eaten==false)
         {
+            Eaten = true;
+            Instantiate(Particle, transform.position, transform.rotation);
             Debug.Log("food eaten");
-            Destroy(this.gameObject);
+            StartCoroutine(Eating());
+            Destroy(this.gameObject,1f);
+        }
+    }
+
+    IEnumerator Eating()
+    {
+        float alpha = 1;
+        for (int i = 0; i < 20; i++)
+        {
+            alpha -= 0.05f;
+            MR.material.color = new Color(1f, 1f, 1f, alpha);
+            yield return new WaitForSeconds(0.05f);            
+            
         }
     }
 }
